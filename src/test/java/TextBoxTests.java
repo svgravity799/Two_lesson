aab2865c5e279b13876de8f94741e615066c4e3d
Pavel Ya. <svgravity799@gmail.com>
2024-11-30 16:02:40 +0300

    added JUnitExamples26

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -8,13 +7,13 @@ public class TextBoxTests {
 
     @Test
     void fillFormTest() {
-open("https://demoqa.com/text-box");
-$("[id=userName]").setValue("Pavel Yatmanov");
-$("[id=userEmail]").setValue("svgravity799@gmail.com");
-$("[id=currentAddress]").setValue("Ulyanovsk");
-$("[id=permanentAddress]").setValue("Ulica Ablukova");
-$("[id=submit]").click();
-$("[id=output]").shouldHave(text("Ulica Ablukova"));
+        open("https://demoqa.com/text-box");
+        $("[id=userName]").setValue("Pavel Yatmanov");
+        $("[id=userEmail]").setValue("svgravity799@gmail.com");
+        $("[id=currentAddress]").setValue("Ulyanovsk");
+        $("[id=permanentAddress]").setValue("Ulica Ablukova");
+        $("[id=submit]").click();
+        $("[id=output]").shouldHave(text("Ulica Ablukova"));
 
 
 
